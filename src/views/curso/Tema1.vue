4d8456699eb7d399aa1607dd18a5cedaab64816a
flizcanor <flizcanor@icloud.com>
2025-01-07 19:18:45 -0500

    se finaliza tema 1

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -3,8 +3,128 @@
   BannerInterno(:subTitulo="'1. Definición y objetivos de la microeconomía'")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
 
+
+    .bg-color-1.mb-5(data-aos="fade-up")
+      .row.justify-content-center.align-items-center
+        .col-lg-auto
+          figure
+            img(src='@/assets/curso/tema1/1.png', alt='')
+        .col-lg
+          .p-4
+            p.mb-0(data-aos="fade-up") La microeconomía es una rama esencial de la economía, que se centra en el análisis del comportamiento de los agentes individuales, como consumidores, empresas y gobiernos, dentro del sistema económico. Este enfoque permite entender cómo toman decisiones sobre el uso eficiente de recursos limitados y cómo estas decisiones afectan los precios, la producción y el bienestar general. A lo largo de este tema, se explorarán los conceptos fundamentales de la microeconomía, sus objetivos y sus aplicaciones, en contextos cotidianos y profesionales.
+
+
     #t_1_1.titulo-segundo(data-aos="flip-up")
-      h2 #[span 1.1] Subtítulo tema
+      h2 #[span 1.1] Definición de microeconomía y su relación con la economía general
+
+
+    .row.mb-5
+      .col-lg-8
+        p(data-aos="fade-left") Para poder hablar sobre microeconomía, primero debemos conocer que es la economía en sí; para Rodríguez Galván en su libro Microeconomía: teoría y ejercicios, la economía es: “La ciencia que estudia la extracción, producción, intercambio y consumo de bienes y servicios escasos, así como su correcta distribución para satisfacer las necesidades ilimitadas de las familias, empresas y gobiernos”. (Pag 15). 
+
+        .bg-color-1.p-4(data-aos="fade-left")
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/icon-1.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 El objetivo de la economía es el de poder analizar las decisiones que toman los agentes económicos, entre los escasos recursos de los que se dispone en ese entonces y las necesidades que se tienen para ser cubiertas. 
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/2.png", data-aos="zoom-in")
+
+
+    p(data-aos="fade-left") #[strong La economía se divide en dos ramas muy importantes, las cuales son: ]
+
+    .bg-full-width.bg-fondo-2
+      .px-4.px-md-5.pb-md-3
+
+        .row.justify-content-center.align-items-stretch.mb-4
+          .col-lg-6.mb-4(data-aos="zoom-in-up")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema1/3.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Microeconomía
+                p.mb-0 Estudia la manera en que las personas, familias, gobiernos o las empresas, toman decisiones respecto a sus recursos y cómo interaccionan en mercados específicos.
+          .col-lg-6.mb-4(data-aos="zoom-in-down")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema1/4.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Macroeconomía
+                p.mb-0 Estudia los fenómenos económicos como un todo y sus repercusiones en el desarrollo económico global (desempleo, inflación y crecimiento económico).
+
+        p(data-aos="fade-left") Luego de conocer un poco sobre qué es la economía, nos vamos a centrar en una de las ramas que anteriormente hablábamos, que es la #[strong microeconomía.]
+
+
+    .bg-full-width.bg-h85.mb-5
+      .px-4.px-md-5
+
+        .row.justify-content-center.align-items-center
+          .col-lg-12
+            .row.justify-content-center.align-items-center
+              .col-lg-5(data-aos="zoom-in")
+                img.img-fluid(src='@/assets/curso/tema1/5.svg', style="max-width:505px", alt="").mx-auto.mx-lg-0.ms-lg-auto.mb-4.mb-lg-0
+
+              .col-lg-7(data-aos="fade-left")
+                p(data-aos="fade-left").mb-5 La microeconomía se define como el estudio de cómo los individuos y las organizaciones toman decisiones sobre la asignación de recursos limitados, para satisfacer necesidades y deseos ilimitados. Mientras que la macroeconomía se concentra en aspectos amplios como el crecimiento económico y la inflación, la microeconomía se centra en aspectos más específicos, como el comportamiento del consumidor y la teoría de la producción (Antelo -Suárez, 2015).
+
+                .bg-color-4.p-4.p-lg-5
+                  p.mb-0.text-white #[b Por ejemplo], cuando se decide cómo gastar el presupuesto mensual, se están utilizando principios microeconómicos. La microeconomía busca explicar cómo se establecen los precios de los bienes y servicios, cómo se asignan los recursos entre los diferentes usos y cómo los mercados se ajustan, entre los cambios en la oferta y la demanda.]
+
+
+    .row.mb-5
+      .col-lg-8
+        p(data-aos="fade-left").mb-4 La relación entre microeconomía y economía general, es estrecha. Ambas disciplinas se complementan: la microeconomía aporta una visión detallada y específica, mientras que la macroeconomía interpreta fenómenos globales a partir de datos agregados. Por ejemplo, las decisiones individuales de consumo y ahorro, estudiadas en la microeconomía, impactan directamente en el nivel general de demanda agregada, un concepto clave en la macroeconomía (Salas Velasco, 2018).
+
+        .bg-color-1.p-4(data-aos="fade-left")
+          p.mb-0 Esto invita a cuestionar la validez de basar distinciones morales en diferencias naturales, lo que a menudo resulta en una división desigual de roles, según el sexo; además, nos invita a reconocer y valorar la diversidad para promover la convivencia pacífica y el respeto mutuo en una sociedad plural.
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/6.png", data-aos="zoom-in")
+
+
+    .bg-full-width.bg-color-5.mb-5
+      .px-4.px-md-5.py-4
+        .row.align-items-center
+          .col-lg-auto
+            img(src="@/assets/curso/tema1/icon-2.svg", style="max-width: 90px").mx-auto
+          .col-lg
+            p.mb-0 La microeconomía es fundamental para comprender las dinámicas de los mercados y para prever cómo las decisiones individuales, afectan el sistema económico general. Este enfoque ayudará a relacionar las elecciones económicas diarias, con el panorama global, brindando herramientas para tomar decisiones más informadas y estratégicas.
+
+    #t_1_2.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.2] Principales objetivos de la microeconomía
+
+
+    p(data-aos="fade-left") La microeconomía, como rama de la economía, se enfoca en estudiar cómo los agentes económicos toman decisiones para asignar escasos recursos, entre múltiples alternativas. Este análisis detallado tiene como objetivo principal, optimizar el uso de los recursos y comprender las dinámicas que determinan los precios y las cantidades en los mercados.
+
+    p(data-aos="fade-left") #[b Los principales objetivos de la microeconomía, son: ]
+
+    .bg-full-width.bg-color-info
+      .p-4.p-md-5
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-10
+            ImagenInfografica.color-secundario
+                template(v-slot:imagen)
+                  figure
+                    img(src='@/assets/curso/tema1/info1.png', alt='', style="max-width: 874px;").mx-auto
+
+                .bg-color-white.box-shadow.p-3(x="20%" y="55%" numero="+")
+                  h5 Determinar precios y cantidades en los mercados
+                  p Esto incluye comprender cómo las fuerzas de la oferta y la demanda interactúan para establecer precios y cantidades de equilibrio (Vial Lira, B. 2012). Por ejemplo, en un mercado de alimentos, el precio de la fruta puede depender de factores como los costos de producción y las preferencias de los consumidores.
+                  p La microeconomía examina cómo los precios de los factores de producción, como el trabajo y el capital, afectan las decisiones de las empresas. Estas decisiones impactan no solo la rentabilidad empresarial, sino también el bienestar de los consumidores, al determinar qué bienes se producen y en qué cantidades (Rodríguez Galván, 2022). En este sentido, la microeconomía se convierte en una herramienta clave para analizar y prever cómo las variaciones en los precios, afectan a los mercados y a los actores que participan en ellos.
+
+                .bg-color-white.box-shadow.p-3(x="42%" y="15%" numero="+")
+                  h5 Asignación eficiente de recursos escasos
+                  p Este objetivo se basa en un análisis crucial en un contexto donde las necesidades humanas son ilimitadas, pero los recursos son finitos. Por ejemplo, en una economía, los agricultores deben decidir si destinan su tierra al cultivo de alimentos básicos o de productos con mayor valor en el mercado.
+                  p La microeconomía busca identificar las condiciones bajo las cuales los recursos pueden ser asignados de manera eficiente, maximizando el bienestar, tanto de los consumidores como de los productores. Esto incluye analizar cómo los cambios en los precios o las políticas públicas, como los subsidios, pueden influir en la asignación de recursos.
+
+                .bg-color-white.box-shadow.p-3(x="78.5%" y="45%" numero="+")
+                  h5 Análisis de los modelos de mercado
+                  p Cada tipo de mercado presenta dinámicas particulares que afectan la fijación de precios, la producción y el consumo. Por ejemplo, en un mercado de competencia perfecta, con muchos compradores y vendedores, los precios son establecidos por las fuerzas de la oferta y la demanda. En cambio, en un monopolio, un único vendedor controla el mercado, pudiendo establecer precios más altos.
+                  p Comprender estos modelos, permite analizar cómo las estructuras de mercado influyen en la eficiencia económica y en el bienestar social. Además, ayuda a los gobiernos a diseñar políticas que promuevan mercados más justos y competitivos, como leyes antimonopolio o regulaciones, para evitar los abusos de poder de mercado.
+                .bg-color-white.box-shadow.p-3(x="58%" y="83%" numero="+")
+                  h5 Impacto de las decisiones individuales en el bienestar colectivo
+                  p Este objetivo incluye analizar el concepto de excedente del consumidor y del productor, que mide los beneficios que obtienen los agentes económicos en un mercado. Por ejemplo, un consumidor puede obtener un excedente si paga menos de lo que estaba dispuesto a pagar por un bien, mientras que un productor lo obtiene cuando vende un bien por encima de su costo de producción.
+
 
     .bg-full-width.border-top.color-primario
       .p-4.p-md-5
@@ -15,15 +135,28 @@
 
             p.d-flex.my-4
               img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
-              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/274611" target="_blank" rel="noopener noreferrer") De Castro Emilio Cerdá, F. J. A. L. M. (s/f). Las energías renovables en el ámbito internacional. Revistasice.com. 
+              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/224089" target="_blank" rel="noopener noreferrer") Rodríguez Galván, J. Y. (2022). Microeconomía: teoría y ejercicios. Grupo Editorial Éxodo. 
 
             p.d-flex.my-4
-              img.me-3(src='@/assets/template/book.svg' :style="{'max-width':'16px'}")
-              span De Janeiro, R. (1992). La declaración del Río sobre el Medio Ambiente y el Desarrollo. Río de Janeiro República Federativa del Brasil., aprobada en Estocolmo el, 16.
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/170085" target="_blank" rel="noopener noreferrer") Antelo-Suárez, M. (2015). Microeconomía intermedia en casos. Delta Publicaciones. 
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/105577" target="_blank" rel="noopener noreferrer") Salas Velasco, M. (2018). Microeconomía: conceptos teóricos y aplicaciones. Difusora Larousse - Ediciones Pirámide. 
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/67738" target="_blank" rel="noopener noreferrer") Vial Lira, B. (2012). Microeconomía. Editorial ebooks Patagonia - Ediciones Universidad Católica de Chile. 
 
             p.d-flex.my-4
               img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
-              a(href="https://youtu.be/QKLjwCMyv3M?si=uAWhO9tnyghljaJg" target="_blank" rel="noopener noreferrer") Innovación y sostenibilidad.
+              a(href="https://www.youtube.com/watch?v=3nZ18l5sNs4" target="_blank" rel="noopener noreferrer") ecovi. (2011). Conceptos Básicos sobre Microeconomía
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
+              a(href="https://www.youtube.com/watch?v=tKZWgW6dSjI" target="_blank" rel="noopener noreferrer") Franco, J. C. A. [@JuanCarlosAguadoFranco]. (2013). Objeto de estudio de la microeconomía (video).
+
 
           .col-12.col-md-6.col-lg-3.offset-lg-1
             figure
